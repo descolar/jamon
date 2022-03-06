1c5414025e972d53ff125e3a67efe192900ca5e9
david <david.escolar@kubide.es>
2022-03-06 10:29:03 +0100

    Proyecto EscolarGodinezDavid2122 comentado con javadoc

--- src/jamon/JamonEscolarGodinezDavid2122.java
+++ src/jamon/JamonEscolarGodinezDavid2122.java
@@ -8,63 +8,117 @@ package jamon;
  *
  * @author portatil_profesorado
  */
+/**
+ * Clase abstracta para representar un jamón.
+ */
 public class JamonEscolarGodinezDavid2122 {
-    
+
+    /**
+     * Categoría del jamón.
+     */
     private String categoria;
+    /**
+     * Precio del jamón.
+     */
     private double precio;
+    /**
+     * Meses de curación del jamón.
+     */
     private int mesesCuracion;
+    /**
+     * Stock actual del jamón.
+     */
     private int stock;
 
-    /* Constructor sin argumentos */
-    public JamonEscolarGodinezDavid2122 ()
-    {
+    /**
+     *
+     * Metodo constructor por defecto sin argumentos
+     *
+     */
+    public JamonEscolarGodinezDavid2122() {
     }
-    // Constructor con parámetro para iniciar todas las propiedades de la clase
-    // jamon
-    
-    public JamonEscolarGodinezDavid2122 (String cat, double precio, int stock)
-    {
-        this.categoria =cat;
-        this.precio=precio;
-        this.stock=stock;
+
+    /**
+     *
+     * Constructor con parámetro para iniciar todas las propiedades de la clase
+     *
+     * @param cat Categoria del jamón
+     * @param precio Precio del jamón
+     * @param stock Stock inicial del jamón
+     *
+     */
+    public JamonEscolarGodinezDavid2122(String cat, double precio, int stock) {
+        this.categoria = cat;
+        this.precio = precio;
+        this.stock = stock;
     }
-   // Método para asignar la categoría del jamón
-    public void asignarCategoria(String cat)
-    {
+
+    /**
+     *
+     * Método para asignar la categoría del jamón
+     *
+     * @param cat Categoria del jamón
+     *
+     */
+    public void asignarCategoria(String cat) {
         setCategoria(cat);
     }
+
     // Método que me devuelve la categoría del jamón
-    public String obtenerCategoria()
-    {
+    public String obtenerCategoria() {
         return getCategoria();
     }
 
-    // Método que me devuelve el stock de jamones disponible en cada momento
-     public int obtenerStock ()
-    {
+    /**
+     *
+     * Método que me devuelve el stock de jamones disponible en cada momento
+     *
+     * @return Devuelve stock actual de jamones en ese momento
+     *
+     */
+    public int obtenerStock() {
         return getStock();
     }
 
-    /* Método para comprar coches. Modifica el stock.
-     * Este método va a ser probado con Junit
+    /**
+     *
+     * Método para comprar jamones. Modifica el stock Este método va a ser
+     * probado con Junit
+     *
+     * @param cantidad Cantidad de jamones a comprar
+     * @throws Exception Si cantidad de jamones a comprar es inferior a 0
+     *
      */
-    public void comprar(int cantidad) throws Exception
-    {
-        if (cantidad<0)
+    public void comprar(int cantidad) throws Exception {
+        if (cantidad < 0) {
             throw new Exception("No se puede comprar un nº negativo de jamones");
+        }
         setStock(getStock() + cantidad);
     }
 
-    public void vender (int cantidad) throws Exception
-    {
-        if (cantidad <= 0)
-            throw new Exception ("No se puede vender una cantidad negativa de jamones");
-        if (obtenerStock()< cantidad)
-            throw new Exception ("No  hay suficientes jamones para vender");
+    /**
+     *
+     * Método para vender jamones. Modifica el stock Este método va a ser
+     * probado con Junit
+     *
+     * @param cantidad Cantidad de jamones a vender
+     * @throws Exception Si cantidad de jamones a vender es inferior a 0
+     * @throws Exception Si cantidad de jamones a vender supera stock actual
+     *
+     */
+    public void vender(int cantidad) throws Exception {
+        if (cantidad <= 0) {
+            throw new Exception("No se puede vender una cantidad negativa de jamones");
+        }
+        if (obtenerStock() < cantidad) {
+            throw new Exception("No  hay suficientes jamones para vender");
+        }
         setStock(getStock() - cantidad);
     }
 
     /**
+     * Getter Devuelve la categoria del jamón
+     *
      * @return the categoria
      */
     public String getCategoria() {
@@ -72,6 +126,8 @@ public class JamonEscolarGodinezDavid2122 {
     }
 
     /**
+     * Setter Establece la categoria del jamón
+     *
      * @param categoria the categoria to set
      */
     public void setCategoria(String categoria) {
@@ -79,6 +135,8 @@ public class JamonEscolarGodinezDavid2122 {
     }
 
     /**
+     * Getter Devuelve el precio del jamón
+     *
      * @return the precio
      */
     public double getPrecio() {
@@ -86,6 +144,8 @@ public class JamonEscolarGodinezDavid2122 {
     }
 
     /**
+     * Setter Establece el precio del jamón
+     *
      * @param precio the precio to set
      */
     public void setPrecio(double precio) {
@@ -93,6 +153,8 @@ public class JamonEscolarGodinezDavid2122 {
     }
 
     /**
+     * Getter Devuelve los meses de curación
+     *
      * @return the mesesCuracion
      */
     public int getMesesCuracion() {
@@ -100,6 +162,8 @@ public class JamonEscolarGodinezDavid2122 {
     }
 
     /**
+     * Setter Establece los meses de curación del jamón
+     *
      * @param mesesCuracion the mesesCuracion to set
      */
     public void setMesesCuracion(int mesesCuracion) {
@@ -107,6 +171,8 @@ public class JamonEscolarGodinezDavid2122 {
     }
 
     /**
+     * Getter Devuelve el stock
+     *
      * @return the stock
      */
     public int getStock() {
@@ -114,6 +180,8 @@ public class JamonEscolarGodinezDavid2122 {
     }
 
     /**
+     * Setter Establece el valor del stock
+     *
      * @param stock the stock to set
      */
     public void setStock(int stock) {
--- src/jamon/Main.java
+++ src/jamon/Main.java
@@ -5,42 +5,57 @@
 package jamon;
 
 /**
+ * Clase Main de la aplicación
  *
  * @author portatil_profesorado
  */
 public class Main {
-    
+
     public static void main(String[] args) {
         JamonEscolarGodinezDavid2122 miJamonEscolarGodinezDavid2122;
         int stockActual;
-        
-        miJamonEscolarGodinezDavid2122 = new JamonEscolarGodinezDavid2122("5Jotas",580,100);
-       
+
+        miJamonEscolarGodinezDavid2122 = new JamonEscolarGodinezDavid2122("5Jotas", 580, 100);
+
         vendeJamEscolarGodinezDavid2122(miJamonEscolarGodinezDavid2122);
-        
+
         compraJamEscolarGodinezDavid2122(miJamonEscolarGodinezDavid2122);
         stockActual = miJamonEscolarGodinezDavid2122.obtenerStock();
-        System.out.println("El stock actual es "+ stockActual );
+        System.out.println("El stock actual es " + stockActual);
     }
 
+    /**
+     *
+     * Método que permite la compra de jamones y muestra por consola si la venta
+     * se ha realizado correctamente o si ha habido algun fallo
+     *
+     * @param cantidad Cantidad de jamones a comprar
+     * @throws Exception Si cantidad de jamones a comprar es inferior a 0
+     *
+     */
     private static void compraJamEscolarGodinezDavid2122(JamonEscolarGodinezDavid2122 miJamonEscolarGodinezDavid2122) {
-        try
-        {
+        try {
             System.out.println("Compra de Jamones");
             miJamonEscolarGodinezDavid2122.comprar(50);
-        } catch (Exception e)
-        {
+        } catch (Exception e) {
             System.out.print("Fallo al ingresar " + e);
         }
     }
 
+    /**
+     *
+     * Método que permite la venta de jamones y muestra por consola si la venta
+     * se ha realizado correctamente o si ha habido algun fallo
+     *
+     * @param cantidad Cantidad de jamones a comprar
+     * @throws Exception Si cantidad de jamones a comprar es inferior a 0
+     *
+     */
     private static void vendeJamEscolarGodinezDavid2122(JamonEscolarGodinezDavid2122 miJamonEscolarGodinezDavid2122) {
-        try
-        {
+        try {
             System.out.println("Venta de Jamones");
             miJamonEscolarGodinezDavid2122.vender(80);
-        } catch (Exception e)
-        {
+        } catch (Exception e) {
             System.out.print("Fallo al vender " + e);
         }
     }
